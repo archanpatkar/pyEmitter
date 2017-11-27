8249fa925956daa22e16dc56b8ea7624a92c0750
Archan Patkar <archanpatkar@gmail.com>
2017-11-27 21:48:24 +0530

    Created emitter.py

---
+++ emitter.py
@@ -0,0 +1,19 @@
+from threading import Thread
+
+class EventEmitter(Thread):
+    def __init__(self):
+        self.emitter = {}
+
+    def on(self,event,f):
+        if(self.emitter.get(event) == None):
+            self.emitter[event] = []
+            self.emitter[event].append(f)
+        else:
+            self.emitter[event].append(f)
+
+    def emit(self,event,*args):
+        consumers = self.emitter.get(event)
+        if(consumers == None):
+            pass
+        else:
+            [consumer(*args) for consumer in consumers]
